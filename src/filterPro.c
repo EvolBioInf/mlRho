5046f474b814b5e8ed17c37051e708937a30c8b4
Fabian Klötzl <kloetzl@evolbio.mpg.de>
2014-07-09 15:08:48 +0200

    cleanup filterPro.c
     * fixes indentation
     * fixes types (int -> size_t)
     * fixes comparison function
     * fixes memory leak
     * rewrote some code

--- src/filterPro.c
+++ src/filterPro.c
@@ -5,65 +5,74 @@
  **************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "profile.h"
 #include "mlComp.h"
 #include "eprintf.h"
 
-int compDouble(const void *a, const void *b);
+int ascCompDouble(const void *a, const void *b);
+int descCompDouble(const void *a, const void *b);
 
 char *filterPro(double pi, double inclFract){
-  Profile *profiles;
-  int i, numProfiles;
-  double *lOnes, *lTwos;
-  double *lik, *originalLik, piComp, sum;
-  double minLik, minSum;
-  char *incl;
+	Profile *profiles;
+	size_t i, numProfiles;
+	double *lik, *originalLik, piComp, sum;
+	double minLik, minSum;
+	char *incl;
 
-  numProfiles = getNumProfiles();
+	numProfiles = (size_t) getNumProfiles();
 
-  incl = (char *)emalloc(numProfiles*sizeof(char));
-  if(inclFract == 1.0){
-    for(i=0;i<numProfiles;i++)
-      incl[i] = 1;
-    return incl;
-  }
-  profiles = getProfiles();
-  lOnes = getLones();
-  lTwos = getLtwos();
+	incl = (char *) emalloc(numProfiles*sizeof(char));
+	if(inclFract == 1.0){
+		memset( incl, 1, numProfiles);
+		return incl;
+	}
 
-  lik = (double *)emalloc(numProfiles*sizeof(double));
-  originalLik = (double *)emalloc(numProfiles*sizeof(double));
-  piComp = 1. - pi;
-  sum = 0.0;
-  for(i=0;i<numProfiles;i++){
-    lik[i] = -log(lOnes[i]*piComp + lTwos[i]*pi) * profiles[i].n;
-    originalLik[i] = lik[i];
-    sum += lik[i];
-  }
-  qsort(lik,numProfiles,sizeof(double),compDouble);
-  minSum = sum * inclFract;
-  sum = 0;
-  i = 0;
-  while(sum < minSum)
-    sum += lik[i++];
-  minLik = lik[i-1];
-  for(i=0;i<numProfiles;i++)
-    if(originalLik[i] >= minLik)
-      incl[i] = 1;
-    else
-      incl[i] = 0;
-  return incl;
+	profiles = getProfiles();
+
+	double const *lOnes = getLones();
+	double const *lTwos = getLtwos();
+
+	lik = (double *) emalloc(numProfiles*sizeof(double));
+	originalLik = (double *) emalloc(numProfiles*sizeof(double));
+
+	piComp = 1.0 - pi;
+	sum = 0.0;
+	for(i = 0; i < numProfiles; i++){
+		lik[i] = -log(lOnes[i]*piComp + lTwos[i]*pi) * profiles[i].n;
+		originalLik[i] = lik[i];
+		sum += lik[i];
+	}
+
+	qsort( lik, numProfiles, sizeof(double), descCompDouble);
+
+	minSum = sum * inclFract;
+	sum = 0.0;
+	for(i = 0; sum < minSum; i++){
+		sum += lik[i];
+	}
+
+	minLik = lik[i-1];
+	for(i = 0; i < numProfiles; i++){
+		incl[i] = originalLik[i] >= minLik ? 1 : 0;
+	}
+
+	free(originalLik);
+	free(lik);
+	return incl;
 }
 
-int compDouble(const void *a, const void *b){
-  double x;
+// This function is for ascending sorting. The original implementaion
+// of compDouble was wrong.
+int ascCompDouble(const void *a, const void *b){
+	const double aa = *((double *) a);
+	const double bb = *((double *) b);
 
-  x = *((double *) b) - *((double *) a); 
+	if( aa > bb ) return 1;
+	if( aa < bb ) return -1;
+	return 0;
+}
 
-  if(x > 0)
-    return 1;
-  else if(x < 0)
-    return -1;
-  else
-    return 0;
+int descCompDouble(const void *a, const void *b){
+	return -ascCompDouble(a,b);
 }
