019b2d6a7e922f017810305a06fc8c28fd8c9909
Fabian Klötzl <kloetzl@evolbio.mpg.de>
2014-07-09 13:53:30 +0200

    fixes indentation

--- src/ld.c
+++ src/ld.c
@@ -14,59 +14,62 @@
 ContigDescr *thisContigDescr = NULL;
 
 void setContigDescr(ContigDescr *contigDescr){
-  thisContigDescr = contigDescr;
+	thisContigDescr = contigDescr;
 }
 
 
 FILE *iniLdAna(Args *args){
-  char *fileName;
-  char *tag;
-  FILE *fp;
-  ContigDescr *cp;
-  int i, max, n, numRead;
+	char *fileName;
+	char *tag;
+	FILE *fp;
+	ContigDescr *cp;
+	int i, max, n, numRead;
 
-  /* get contig lengths from file */
-  tag = (char *)emalloc(4*sizeof(char));
-  fileName = (char *)emalloc(256*sizeof(char));
-  fileName = strcpy(fileName,args->n);
-  fileName = strcat(fileName,".con");
-  fp = efopen(fileName,"rb");
-  numRead = fread(tag,sizeof(char),3,fp);
-  assert(numRead == 3);
-  tag[3] = '\0';
-  if(strcmp(tag,"con") != 0)
-    assert(0);
-  cp = (ContigDescr *)emalloc(sizeof(ContigDescr));
-  numRead = fread(&n,sizeof(int),1,fp);
-  assert(numRead == 1);
-  cp->pos = NULL;
-  cp->n = n;
-  cp->len = (int *)emalloc(n*sizeof(int));
-  for(i=0;i<cp->n;i++){
-    numRead = fread(&cp->len[i],sizeof(int),1,fp);
-    assert(numRead == 1);
-  }
-  fclose(fp);
-  max = 0;
-  for(i=0;i<n;i++){
-    if(cp->len[i] > max)
-      max = cp->len[i];
-  }
-  cp->posBuf = (Position *)emalloc(max*sizeof(Position));
-  setContigDescr(cp);
+	int check;
 
-  /* get file pointer for position file */
-  fileName = strcpy(fileName,args->n);
-  fileName = strcat(fileName,".pos");
-  fp = efopen(fileName,"rb");
-  numRead = fread(tag,sizeof(char),3,fp);
-  if(strcmp(tag,"pos") != 0)
-    assert(0);
-  free(fileName);
-  free(tag);
-  return fp;
+	/* get contig lengths from file */
+	tag = (char *)emalloc(4*sizeof(char));
+	
+	check = asprintf( &fileName, "%s.con", args->n);
+	assert( check != -1);
+
+	fp = efopen(fileName,"rb");
+	numRead = fread(tag,sizeof(char),3,fp);
+	assert(numRead == 3);
+	tag[3] = '\0';
+	if(strcmp(tag,"con") != 0)
+		assert(0);
+	cp = (ContigDescr *)emalloc(sizeof(ContigDescr));
+	numRead = fread(&n,sizeof(int),1,fp);
+	assert(numRead == 1);
+	cp->pos = NULL;
+	cp->n = n;
+	cp->len = (int *)emalloc(n*sizeof(int));
+	for(i=0;i<cp->n;i++){
+		numRead = fread(&cp->len[i],sizeof(int),1,fp);
+		assert(numRead == 1);
+	}
+	fclose(fp);
+	max = 0;
+	for(i=0;i<n;i++){
+		if(cp->len[i] > max)
+			max = cp->len[i];
+	}
+	cp->posBuf = (Position *)emalloc(max*sizeof(Position));
+	setContigDescr(cp);
+
+	/* get file pointer for position file */
+	fileName = strcpy(fileName,args->n);
+	fileName = strcat(fileName,".pos");
+	fp = efopen(fileName,"rb");
+	numRead = fread(tag,sizeof(char),3,fp);
+	if(strcmp(tag,"pos") != 0)
+		assert(0);
+	free(fileName);
+	free(tag);
+	return fp;
 }
 
 ContigDescr *getContigDescr(){
-  return thisContigDescr;
+	return thisContigDescr;
 }
