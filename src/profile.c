9a9be7b298086cc454e6d97087811811696e95dc
Fabian Klötzl <kloetzl@evolbio.mpg.de>
2014-07-09 14:15:41 +0200

    cleanup profile.c

--- src/profile.c
+++ src/profile.c
@@ -17,61 +17,64 @@ Profile *thisProfiles;
 int thisNumProfiles;
 
 void readProfiles(char *baseName){
-  char *fileName, tag;
-  int i, numProfiles, numRead, sop;
-  Profile *profiles;
-  FILE *fp;
+	char *fileName, tag;
+	int i, numProfiles, numRead;
+	Profile *profiles;
+	FILE *fp;
 
-  fileName = (char *)emalloc(256*sizeof(char));
-  fileName = strcpy(fileName,baseName);
-  fileName = strcat(fileName,".sum");
+	int check = asprintf( &fileName, "%s.sum", baseName);
 
-  fp = efopen(fileName,"rb");
-  for(i=0;i<3;i++)
-    numRead = fread(&tag,sizeof(char),1,fp);
-  assert(numRead == 1);
-  numRead = fread(&numProfiles,sizeof(int),1,fp);
-  assert(numRead == 1);
-  sop = sizeof(Profile);
-  profiles = (Profile *)emalloc(numProfiles*sop);
-  for(i=0;i<numProfiles;i++){
-    numRead = fread(&profiles[i],sop,1,fp);
-    assert(numRead == 1);
-  }
-  setProfiles(profiles);
-  setNumProfiles(numProfiles);
-  fclose(fp);
-  free(fileName);
+	fp = efopen(fileName,"rb");
+
+	// FIXME: The following loop looks like a bug to me. Not only does it read the
+	// same character thrice; Also the resulting `tag` is never used.
+	for(i=0;i<3;i++)
+		numRead = fread(&tag,sizeof(char),1,fp);
+	assert(numRead == 1);
+
+	numRead = fread(&numProfiles,sizeof(int),1,fp);
+	assert(numRead == 1);
+
+	profiles = (Profile *) emalloc(numProfiles * sizeof(Profile));
+	for(i = 0; i < numProfiles; i++){
+		numRead = fread( &profiles[i], sizeof(Profile), 1, fp);
+		assert(numRead == 1);
+	}
+
+	setProfiles(profiles);
+	setNumProfiles(numProfiles);
+	fclose(fp);
+	free(fileName);
 }
 
 void setProfiles(Profile *profiles){
-  thisProfiles = profiles;
+	thisProfiles = profiles;
 }
 
 Profile *getProfiles(){
-  return thisProfiles;
+	return thisProfiles;
 }
 
 void setNumProfiles(int numProfiles){
-  thisNumProfiles = numProfiles;
+	thisNumProfiles = numProfiles;
 }
 
 int getNumProfiles(){
-  return thisNumProfiles;
+	return thisNumProfiles;
 }
 
 double getCoverage(){
-  long i, j;
-  double c, sum, numProf;
+	long i, j;
+	double c, sum, numProf;
 
-  sum = 0;
-  numProf = 0;
-  for(i=0;i<thisNumProfiles;i++){
-    c = 0;
-    for(j=0;j<4;j++)
-      c += thisProfiles[i].profile[j];
-    sum += thisProfiles[i].n * c;
-    numProf += thisProfiles[i].n;
-  }
-  return sum/numProf;
+	sum = 0;
+	numProf = 0;
+	for(i=0;i<thisNumProfiles;i++){
+		c = 0;
+		for(j=0;j<4;j++)
+			c += thisProfiles[i].profile[j];
+		sum += thisProfiles[i].n * c;
+		numProf += thisProfiles[i].n;
+	}
+	return sum/numProf;
 }
